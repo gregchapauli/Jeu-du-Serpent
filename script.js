658212f0a69e2b011dfe62cb52700b4f4803e148
Gregory Caloin <gregchapauli@gmail.com>
2020-05-27 13:10:11 +0200

    Style complete

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ window.onload = function()
         var canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
-        canvas.style.border = "1px solid";
+        canvas.style.border = "30px solid gray";
+        canvas.style.margin = "50px auto";
+        canvas.style.display = "block";
+        canvas.style.backgroundColor = "#ddd";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
         snakee = new Snake ([[6,4], [5,4], [4,4], [3,4],[2,4]], "right");
@@ -47,9 +50,9 @@ window.onload = function()
                 while(applee.isOnSnake(snakee))
             }
             ctx.clearRect(0,0,canvasWidth,canvasHeight);
+            drawScore();
             snakee.draw();
             applee.draw();
-            drawScore();
             setTimeout(refreshCanvas,delay);
         }
     }
@@ -57,8 +60,20 @@ window.onload = function()
     function gameOver()
     {
         ctx.save();
-        ctx.fillText("Game Over", 5, 15);
-        ctx.fillText("Appuyer sur la touche Espace pour rejouer", 5, 30);
+        ctx.font = "bold 70px sans-serif";
+        ctx.fillStyle = "#000";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.strokeStyle = "White";
+        ctx.lineWidth = 5;
+        var centreX = canvasWidth / 2;
+        var centreY = canvasHeight / 2;
+        ctx.strokeText("Game Over", centreX, centreY - 180);
+        ctx.fillText("Game Over", centreX, centreY - 180);
+
+        ctx.font = "bold 30px sans-serif";
+        ctx.strokeText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120);
+        ctx.fillText("Appuyer sur la touche Espace pour rejouer", centreX, centreY - 120);
         ctx.restore();
     }
 
@@ -73,7 +88,13 @@ window.onload = function()
     function drawScore()
     {
         ctx.save();
-        ctx.fillText(score.toString() , 5, canvasHeight - 5);
+        ctx.font = "bold 200px sans-serif";
+        ctx.fillStyle = "gray";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        var centreX = canvasWidth / 2;
+        var centreY = canvasHeight / 2;
+        ctx.fillText(score.toString() , centreX, centreY);
         ctx.restore();
     }
 
